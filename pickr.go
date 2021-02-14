2f63ac81726b02fe4a8c80f775a5275589731a40
Faithfulness Alamu <vaguemail369@gmail.com>
2021-02-14 13:19:11 +0100

    Switch over possible events in pickr

--- pickr.go
+++ pickr.go
@@ -2,11 +2,39 @@
  */
 package pickr
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 type Event string
 
 type Pickr struct {
 	out   io.Writer
 	event Event
+}
+
+func (p *Pickr) Do(args ...string) error {
+	if p.event == "" {
+		panic("no event specified, there is nothing to do")
+	}
+
+	switch p.event {
+	case EventToss:
+		return p.doToss()
+
+	case EventRoll:
+		var n string
+		if len(args) == 0 {
+			n = "6"
+		}
+		n = args[0]
+		return p.doRoll(n)
+
+	case EventChoose:
+		return p.doChoose(args...)
+
+	default:
+		return fmt.Errorf("unknown event '%s'", p.event)
+	}
 }
