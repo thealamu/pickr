329622731946c45cfddb2c6da29b3b9413f5a874
Faithfulness Alamu <vaguemail369@gmail.com>
2021-02-14 14:07:32 +0100

    Print help usage when no event is specified

--- cmd/pickr/main.go
+++ cmd/pickr/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Pickr")
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "toss":
+		fmt.Println("Tossing a coin")
+	case "roll":
+		fmt.Println("Rolling a die")
+	case "choose":
+		fmt.Println("Choosing from list")
+	}
+}
+
+func printUsage() {
+	fmt.Println(strings.TrimSpace(`
+Usage: pickr event
+Make random choices. Toss a coin, roll a die, choose from a list of args.
+
+Available events:
+	toss		toss a coin
+	roll n		roll a n-sided die
+	choose ARGS...	choose from a list of args
+
+	`))
 }
