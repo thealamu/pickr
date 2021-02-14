8fec44011f95f216bdc63bc8398f06f4ea675edf
Faithfulness Alamu <vaguemail369@gmail.com>
2021-02-14 12:53:26 +0100

    Implement coin toss function

--- toss.go
+++ toss.go
@@ -1,3 +1,15 @@
 package pickr
 
+import "math/rand"
+
 const TossEvent Event = "tossEvent"
+
+// toss implements a single coin toss
+func toss(r rand.Rand) (string, error) {
+	switch r.Intn(2) {
+	case 0:
+		return "Heads", nil
+	default:
+		return "Tails", nil
+	}
+}
