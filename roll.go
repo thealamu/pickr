6e859a6a93d7d25d55f26aa952274385a10b7417
Faithfulness Alamu <vaguemail369@gmail.com>
2021-02-14 13:00:13 +0100

    Implement die roll function

--- roll.go
+++ roll.go
@@ -1,3 +1,13 @@
 package pickr
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 const RollEvent Event = "rollEvent"
+
+// roll implements a single n-sided die roll
+func roll(r rand.Rand, n int) (string, error) {
+	return fmt.Sprintf("%d", r.Intn(n)+1), nil
+}
